afe6245496f0fb06a052fca081e2bdf2403fc875
Dylan Baldi <62406845+cloudnothings123@users.noreply.github.com>
2022-06-24 18:58:35 -0400

    tenth commit

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req,res) => {
+    res.sendFile('/index.ejs')
+})
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Listening...`)
 })
